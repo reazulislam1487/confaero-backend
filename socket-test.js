0f22fc7f1fd959b08bea79ee11d9799f25baf8d0
reazulislam1487 <reazulislam1487@gmail.com>
2026-02-22 14:19:22 +0600

    added some field

--- socket-test.js
+++ socket-test.js
@@ -15,3 +15,12 @@ socket.on("connect", () => {
 socket.on("message:new", (msg) => {
   console.log("📩 New message:", msg.text);
 });
+/* 
+1. i am learning english, so i will write in english to improve my english skills.
+2.i am a software engineer, so i will write about software engineering topics to improve my technical writing skills.
+3. i will write about my daily life and experiences to improve my storytelling skills.
+
+
+
+
+*/
